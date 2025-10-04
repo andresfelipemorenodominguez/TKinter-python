225f3b1990b35b5edfb565f7c201b3b11c8a7f6b
Andrés Felipe Moreno Domínguez <andresdevbod@gmail.com>
2025-10-04 17:36:08 -0500

    init
    init

---
+++ temperatura.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+
+ventana = tk.Tk()  # Crea la ventana principal
+ventana.title("Registro de Usuarios")  # Título de la ventana
+ventana.geometry("300x400")  # Tamaño de la ventana
+
+grados = tk.Label(ventana, text="Ingresa un valor:")
+grados.pack(pady=5)
+entrada_grados = tk.Entry(ventana)
+entrada_grados.pack(pady=5)
+
+def convertirafarenheit():
+    valor = entrada_grados.get()           
+    try:
+        farenheit = float(valor) * 9 / 5 + 32
+        print(farenheit)
+    except ValueError:
+        print("Ingresa un número válido")
+
+botonf = tk.Button(ventana, text="Convertir a Farenheit", command=convertirafarenheit)
+botonf.pack(pady=20)
+ventana.mainloop()
